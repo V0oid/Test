3f53381f82eb4e14d68c1045331f239cc5865141
DRaczek <damian.raczek4206695@gmail.com>
2023-12-11 10:13:47 +0100

    Register page idea implemented.

---
+++ Test/Test/RegisterPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Test
+{
+  
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegisterPage : ContentPage
+    {
+        public ObservableCollection<Sex> Sexes { get; set; } = new ObservableCollection<Sex>()
+        {
+            Sex.Kobieta,
+            Sex.Mezczyzna
+        };
+        public ObservableCollection<MartialStatus> MartialStatuses { get; set; } = new ObservableCollection<MartialStatus>()
+        {
+            MartialStatus.Zajety,
+            MartialStatus.Wolny
+        };
+
+        public RegisterPage()
+        {
+            InitializeComponent();
+            sexEntry.ItemsSource = Sexes;
+            sexEntry.SelectedIndex = 0;
+            martialStatusEntry.ItemsSource = MartialStatuses;
+            martialStatusEntry.SelectedIndex = 0;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
