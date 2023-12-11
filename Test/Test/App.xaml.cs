bea6e030a60f4ea50f6e5d9379d8030c0f3cd2ab
DRaczek <damian.raczek4206695@gmail.com>
2023-12-11 09:45:46 +0100

    User entity & Database class created

--- Test/Test/App.xaml.cs
+++ Test/Test/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -6,11 +7,23 @@ namespace Test
 {
     public partial class App : Application
     {
+        private Database _database;
+        public Database Database
+        {
+            get
+            {
+                if (_database == null)
+                {
+                    _database = new Database(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "db.db3"));
+                }
+                return _database;
+            }
+        }
         public App()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
---
+++ Test/Test/Database.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test
+{
+    public class Database
+    {
+        public SQLiteAsyncConnection database;
+        public Database(string dbPath)
+        {
+            database = new SQLiteAsyncConnection(dbPath);
+        }
+    }
+}
