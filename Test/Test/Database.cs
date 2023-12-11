ea48904c0dfa82c9b3bdc1d6eda3a46c00ee3fda
DRaczek <damian.raczek4206695@gmail.com>
2023-12-11 09:50:45 +0100

    Some database methods created

--- Test/Test/Database.cs
+++ Test/Test/Database.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Test
 {
@@ -11,6 +12,23 @@ namespace Test
         public Database(string dbPath)
         {
             database = new SQLiteAsyncConnection(dbPath);
+        }
+
+        public async Task<bool> isEmailUnique(string email)
+        {
+            var result = await database.Table<User>().Where(u=>u.Email == email).FirstAsync();
+            return result == null ? true : false; 
+        }
+
+        public async Task<int> Register(User user)
+        {
+            return await database.InsertAsync(user);
+        }
+        public async Task<User> Login(string email, string password)
+        {
+            var result = await database.Table<User>().Where(u => u.Email == email
+            && u.Password == password).FirstAsync();
+            return result;
         }
     }
 }
